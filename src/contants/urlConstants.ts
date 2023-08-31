cd6e03c5d97cd14ca7f0bb3ecb2be9236ffae550
poornesh h n <poorneshhn3@gmail.com>
2023-09-01 00:45:16 +0530

    add localhost urls

--- src/contants/urlConstants.ts
+++ src/contants/urlConstants.ts
@@ -1,3 +1,4 @@
-// export const API = "http://localhost:3000/api";
+// export const API = "http://localhost:5000/api";
+// export const AUTH = "http://localhost:5000/auth";
 export const API = "https://z4a6d0286-zf62858b5-gtw.zb20e5e48.qovery.fr/api";
 export const AUTH = "https://z4a6d0286-zf62858b5-gtw.zb20e5e48.qovery.fr/auth";
